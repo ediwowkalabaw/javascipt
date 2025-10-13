03a638c5797e7b068343c9c228e45dab65ac0bf2
ediwowkalabaw <161602343+ediwowkalabaw@users.noreply.github.com>
2025-10-13 13:37:19 +0800

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,21 @@
+let adjective = "fun";
+let noun = "developer";
+let verb = "code";
+let place = "internet";
+let adjective2 = "exciting";
+let noun2 = "projects";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + '.' + " The " + noun + " lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " + verb + '.';
+console.log("First story:" + firstStory);
+
+//new values
+adjective = "brave";    
+noun = "explorer";
+verb = "discover";
+place = "mountains";
+adjective2 = "majestic";
+noun2 = "treasures";
+
+//second story
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + '.' + " The " + noun + " lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " + verb + '.';
+console.log("Second story:" + secondStory);
