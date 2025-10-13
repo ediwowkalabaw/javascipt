27e9af5187b9e453be36b8bcd5730781ee61e58f
ediwowkalabaw <161602343+ediwowkalabaw@users.noreply.github.com>
2025-10-13 13:23:00 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+//greeting message to the user.
+console.log("Hello! I'm your coding fun fact guide!");
+
+//three variables
+let botName = "jbot";
+let botLocation = "the cloud";
+let favoriteLanguage = "javaScript";
+
+//introduction
+console.log("My name is " + botName + "and I live on " + botLocation + ".");
+
+//log
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+//bot favorite coding language
+let codingFact= "one of my most favorite coding languages is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+//another one
+codingFact = "I also like Python with " +favoriteLanguage + ".";
+console.log(codingFact);
+
+//another one 
+codingFact = "I also like C++ with " +favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + "from " + botLocation + ".");
